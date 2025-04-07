771733b187ffc3032c3ba7e5eb4b3aeb093e2b1a
Kolyshkin A <t0l4ik.kas1@gmail.com>
2025-04-07 20:27:38 +0500

    add

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args){
-        System.out.printf("Hello mister !!!");
-        System.out.printf("Hello mister #");
+        System.out.printf("Hello mister !!!!");
     }
 }
